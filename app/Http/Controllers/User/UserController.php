4f66f69ab09c81a49b6522435f1341e6f0218f03
Thiri Sandar Oo <thirisandar4111994@gmail.com>
2017-05-29 16:19:26 +0630

    [Add]add the DockBlock

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -19,20 +19,30 @@ class UserController extends Controller
 {
     private $userRepository;
 
-
+    /**
+     * UserController constructor.
+     * @param UserRepositoryInterface $userRepository
+     * @param checkemail $check
+     */
     public function __construct(UserRepositoryInterface $userRepository,checkemail $check)
-
     {
         $this->check =$check;
         $this->userRepository = $userRepository;
     }
 
-    public function index(){
+    public function index()
+    {
         return view('common/profile');
-
     }
 
-    public function guestprofile($id){
+    /**
+     * @param $id
+     * @return $this|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * This function is to check the other person's profile
+     */
+
+    public function guestprofile($id)
+    {
         if(Auth::user()->id == $id) {
             return view('common/profile');
         }else {
@@ -41,84 +51,82 @@ class UserController extends Controller
         }
     }
 
-    public function register(){
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * This function is to register the user
+     */
+
+    public function register()
+    {
         return view('auth.register');
     }
 
+    /**
+     * @param UserRequest $request
+     * @return mixed
+     * This function is to create the user
+     */
+
     public function insert(UserRequest $request)
-
-
     {
         $request->validate();
         $name =Input::get('name');
         $email = $request->input('email');
         $password = bcrypt($request->input('password'));
         $image = $request->file('image');
-
-
-            $imageName = $image->getClientOriginalName();
-            $destination = 'uploads';
-            $image->move($destination, $imageName);
-            $this->userRepository->insert($name, $imageName, $email, $password);
-        return Redirect::to('login');//            return view('auth/login');
+        $imageName = $image->getClientOriginalName();
+        $destination = 'uploads';
+        $image->move($destination, $imageName);
+        $this->userRepository->insert($name, $imageName, $email, $password);
+        return Redirect::to('login');
         }
 
 
     /**
      * @param UpdateUserRequest $request
      * @return string
+     *
+     * This function is to update the user information
      */
-    public function update(UpdateUserRequest $request){
-
-
+    public function update(UpdateUserRequest $request)
+    {
         $id =Input::get('id');
         $name =Input::get('name');
         $image =Input::file('image');
         $cover_photo=Input::file('cover_photo');
 
+        if(!empty($image) && (!empty($cover_photo)))
+        {
+            $coverName = $cover_photo->getClientOriginalName();
+            $destination = 'uploads';
+            $cover_photo->move($destination, $coverName);
+            $imageName = $image->getClientOriginalName();
+            $destination = 'uploads';
+            $image->move($destination, $imageName);
+            $this->userRepository->allupdate($id,$name, $imageName,$coverName);
 
-        if(!empty($image) && (!empty($cover_photo))) {
-
-                $coverName = $cover_photo->getClientOriginalName();
-                $destination = 'uploads';
-                $cover_photo->move($destination, $coverName);
-                $imageName = $image->getClientOriginalName();
-                $destination = 'uploads';
-                $image->move($destination, $imageName);
-                $this->userRepository->allupdate($id,$name, $imageName,$coverName);
             return redirect()->back();
-            }
 
-        elseif(!empty($image) && (empty($cover_photo))){
-
-
+        } elseif(!empty($image) && (empty($cover_photo)))
+        {
             $imageName = $image->getClientOriginalName();
             $destination = 'uploads';
             $image->move($destination, $imageName);
             $this->userRepository->noCoverUpdate($id,$name, $imageName);
             return redirect()->back();
 
-
-        }
-
-        elseif (empty($image) && (!empty($cover_photo))){
-
+        } elseif (empty($image) && (!empty($cover_photo)))
+        {
             $coverName = $cover_photo->getClientOriginalName();
             $destination = 'uploads';
             $cover_photo->move($destination, $coverName);
             $this->userRepository->noImageUpdate($id,$name,$coverName);
             return redirect()->back();
 
-
-        }
-            else{
+        } else {
                 $this->userRepository->update($id,$name);
                 return redirect()->back();
             }
     }
-
-
-
-
 }
 
--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -14,44 +14,73 @@ use App\users;
 
 class UserRepository implements UserRepositoryInterface{
 
-
-
+    /**
+     * UserRepository constructor.
+     * @param users $users
+     *
+     */
     public function __construct(Users $users)
-
     {
-
         $this->users =$users;
-
-
-
     }
-    public function insert($name,$imageName,$email,$password){
+
+    /**
+     * @param $name
+     * @param $imageName
+     * @param $email
+     * @param $password
+     * This function is to create the customer
+     */
+    public function insert($name,$imageName,$email,$password)
+    {
        $this->users->create(['name'=>$name,'image'=>$imageName,'email'=>$email,'password'=>$password]);
-//
-
-    }
-    public function allupdate($id,$name,$imageName,$coverName){
-
-        $this->users->where('id','=',$id)
-            ->update(['name'=>$name,'image'=>$imageName,'cover_photo'=>$coverName]);
-    }
-
-    public function update($id,$name){
-
-
-      $this->users->where('id','=',$id)->update(['name'=>$name]);
-    }
-    public function noCoverUpdate($id,$name,$imageName){
-        $this->users->where('id','=',$id)
-            ->update(['name'=>$name,'image'=>$imageName]);
-
-    }
-    public function noImageUpdate($id,$name,$coverName){
-        $this->users->where('id','=',$id)
-            ->update(['name'=>$name,'cover_photo'=>$coverName]);
 
     }
 
+    /**
+     * @param $id
+     * @param $name
+     * @param $imageName
+     * @param $coverName
+     * This function is to update the user's all informations.
+     */
+    public function allupdate($id,$name,$imageName,$coverName)
+    {
+        $this->users->where('id','=',$id)->update(['name'=>$name,'image'=>$imageName,'cover_photo'=>$coverName]);
+    }
 
+    /**
+     * @param $id
+     * @param $name
+     *
+     * This function is to update the the name only.
+     */
 
+    public function update($id,$name)
+    {
+        $this->users->where('id','=',$id)->update(['name'=>$name]);
+    }
+
+    /**
+     * @param $id
+     * @param $name
+     * @param $imageName
+     * This function is to update the user information except coverphoto
+     */
+    public function noCoverUpdate($id,$name,$imageName)
+    {
+        $this->users->where('id','=',$id)->update(['name'=>$name,'image'=>$imageName]);
+
+    }
+
+    /**
+     * @param $id
+     * @param $name
+     * @param $coverName
+     * This function is to update the user information except image
+     */
+    public function noImageUpdate($id,$name,$coverName)
+    {
+        $this->users->where('id','=',$id)->update(['name'=>$name,'cover_photo'=>$coverName]);
+    }
 }
